30fe287cebd16ce74c583dc691863f0cba262240
boris <barys.serzhanovich@gmail.com>
2026-09-16 19:39:11 +0300

    Fix #160: keep Space Lens crumbs and map in sync on Cancel

--- Sources/MacClean/Views/Files/SpaceLensNavigation.swift
+++ Sources/MacClean/Views/Files/SpaceLensNavigation.swift
@@ -4,6 +4,7 @@ import Foundation
 /// the back/up/home logic is unit-testable.
 struct SpaceLensNavigation: Equatable {
     private(set) var breadcrumbs: [URL]
+    private var pendingNavigationBreadcrumbs: [URL]?
 
     init(root: URL) { breadcrumbs = [root] }
 
@@ -13,10 +14,42 @@ struct SpaceLensNavigation: Equatable {
     var current: URL { breadcrumbs.last! }
     var canGoUp: Bool { breadcrumbs.count > 1 }
 
-    mutating func drillInto(_ url: URL) { breadcrumbs.append(url) }
-    mutating func up() { if breadcrumbs.count > 1 { breadcrumbs.removeLast() } }
-    mutating func home() { if let root = breadcrumbs.first { breadcrumbs = [root] } }
+    mutating func drillInto(_ url: URL) {
+        beginPendingNavigation()
+        breadcrumbs.append(url)
+    }
+
+    mutating func up() {
+        guard breadcrumbs.count > 1 else { return }
+        beginPendingNavigation()
+        breadcrumbs.removeLast()
+    }
+
+    mutating func home() {
+        guard breadcrumbs.count > 1, let root = breadcrumbs.first else { return }
+        beginPendingNavigation()
+        breadcrumbs = [root]
+    }
+
     mutating func navigate(to url: URL) {
-        if let i = breadcrumbs.firstIndex(of: url) { breadcrumbs = Array(breadcrumbs.prefix(through: i)) }
+        guard let i = breadcrumbs.firstIndex(of: url), i < breadcrumbs.count - 1 else { return }
+        beginPendingNavigation()
+        breadcrumbs = Array(breadcrumbs.prefix(through: i))
+    }
+
+    mutating func commitPendingNavigation() {
+        pendingNavigationBreadcrumbs = nil
+    }
+
+    mutating func cancelPendingNavigation() {
+        guard let previousBreadcrumbs = pendingNavigationBreadcrumbs else { return }
+        breadcrumbs = previousBreadcrumbs
+        pendingNavigationBreadcrumbs = nil
+    }
+
+    private mutating func beginPendingNavigation() {
+        if pendingNavigationBreadcrumbs == nil {
+            pendingNavigationBreadcrumbs = breadcrumbs
+        }
     }
 }
